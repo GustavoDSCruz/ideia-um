38379b61865e275ae75202a7548923886959c1a2
GustavoDSCruz <gustavodiasdsc@gmail.com>
2022-09-11 06:15:51 -0300

    next attemp

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
+import React from 'react';
+import reactDom from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Navbar } from 'react-bootstrap';
 
 
 function Home(){
     return(
         <>
+        <Navbar/>
          <h1>Home</h1>
         </>
         )}
