03bf457ea228f27bdb0993ec2df96e2b7fde0db5
GustavoDSCruz <gustavodiasdsc@gmail.com>
2022-09-12 17:46:41 -0300

    quarta-tentativa

---
+++ components/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import iconHome from './img/logo.png';
+import iconSql from './img/sql.png'
+export default function Navbar(){
+    return (
+        <>
+            <button className="icon-home">
+                <a href="index.html">
+                    <img 
+                    src={iconHome}
+                    className="img-nav"
+                    />SHOP
+                </a>
+            </button>
+            <button><a href="php/cadastrar.php">Cadastrar</a></button>
+            <button><a href="php/listar.php">Listar</a></button>
+            <button><a href="php/alterar.php">Alterar</a></button>
+            <button><a href="php/remover.php">Remover</a></button> 
+            <button><a href="php/consultar.php">Consultar</a></button>
+            <button className="icon-sql">
+                <a target="blank_" href="http://localhost/phpmyadmin/">
+                    <img 
+                    className="img-sql" 
+                    src={iconSql} 
+                    />
+                </a>
+            </button>
+        </>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import reactDom from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import {useState} from 'react';
-
+import Navbar from '../components/Navbar';
 
 
 export default function Home(){
     return(
         <>
-        <p>Home</p>
+        <Navbar />
+        <h1>Home</h1>
         <Contador />
         </>
         )}
